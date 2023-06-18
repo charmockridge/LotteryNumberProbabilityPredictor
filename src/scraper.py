8df7adac0df9481d4b9bb64f8105f07216f93b68
charmockridge <charliemockridge@icloud.com>
2023-06-18 21:20:57 +0100

    refactored code to improve testing

--- src/scraper.py
+++ src/scraper.py
@@ -7,89 +7,96 @@ from datetime import datetime
 
 
 def get_euromillions_draw_history():
-    EUROMILLIONS_URL = "https://www.lottery.co.uk/euromillions/results/archive-"
+    LOTTERY_TYPE = "euromillions"
     EUROMILLIONS_START_YEAR = 2004
     current_year = date.today().year
     data = []
 
     for i in range(current_year, EUROMILLIONS_START_YEAR - 1, -1):
-        response = requests.get(EUROMILLIONS_URL + str(i))
-        soup = BeautifulSoup(response.text, "lxml")
+        soup = get_html("https://www.lottery.co.uk/euromillions/results/archive-" + str(i))
+        data.append(scrape_draw_history(soup, LOTTERY_TYPE))
 
-        table = soup.find("table")
-        rows = table.find_all("tr")[1:]
-        
-        for row in rows:
-            cells = row.find_all("td")
-
-            draw_date = format_draw_date(cells[0].text)
-            drawn_numbers = format_drawn_numbers(cells[1].text)
-
-            if len(cells) == 4:
-                maker_code = format_maker_code(cells[2].text)
-                prize = format_prize(cells[3].text)
-            else:
-                maker_code = ""
-                prize = format_prize(cells[2].text)
-
-            draw = {}
-            draw["draw_date"] = draw_date
-            draw["num_1"] = str(drawn_numbers[0])
-            draw["num_2"] = drawn_numbers[1]
-            draw["num_3"] = drawn_numbers[2]
-            draw["num_4"] = drawn_numbers[3]
-            draw["num_5"] = drawn_numbers[4]
-            draw["bonus_1"] = drawn_numbers[5]
-            draw["bonus_2"] = drawn_numbers[6]
-            draw["millionaire_maker_code"] = maker_code
-            draw["jackpot"] = prize[0]
-            draw["rollover"] = prize[1]
-
-            data.append(draw)
-
-    export_to_xlsx("euromillions", data)
+    export_to_xlsx(LOTTERY_TYPE, data)
 
 
 def get_lotto_draw_history():
-    LOTTO_URL = "https://www.lottery.co.uk/euromillions/results/archive-"
+    LOTTERY_TYPE = "lotto"
     LOTTO_START_YEAR = 1994
     current_year = date.today().year
     data = []
 
     for i in range(current_year, LOTTO_START_YEAR - 1, -1):
-        response = requests.get(LOTTO_URL + str(i))
-        soup = BeautifulSoup(response.text, "lxml")
+        soup = get_html("https://www.lottery.co.uk/euromillions/results/archive-" + str(i))
+        draw = scrape_draw_history(soup, LOTTERY_TYPE)
+        data.append(draw)
 
-        table = soup.find("table")
+    export_to_xlsx("lotto", data)
+
+
+def get_html(URL):
+    reposne = requests.get(URL)
+    return BeautifulSoup(reposne.text, "lxml")
+
+
+def scrape_draw_history(HTML, lottery_type):
+        data = []
+        table = HTML.find("table")
         rows = table.find_all("tr")[1:]
-
+        
         for row in rows:
+            draw = {}
             cells = row.find_all("td")
 
             draw_date = format_draw_date(cells[0].text)
             drawn_numbers = format_drawn_numbers(cells[1].text)
-            prize = format_prize(cells[2].text)
 
-            draw = {}
             draw["draw_date"] = draw_date
-            draw["num_1"] = str(drawn_numbers[0])
-            draw["num_2"] = drawn_numbers[1]
-            draw["num_3"] = drawn_numbers[2]
-            draw["num_4"] = drawn_numbers[3]
-            draw["num_5"] = drawn_numbers[4]
-            draw["num_6"] = drawn_numbers[5]
-            draw["bonus_1"] = drawn_numbers[6]
-            draw["jackpot"] = prize[0]
-            draw["rollover"] = prize[1]
+
+            if lottery_type == "euromillions":
+                if len(cells) == 4:
+                    maker_code = format_maker_code(cells[2].text)
+                    prize = format_prize(cells[3].text)
+                else:
+                    maker_code = ""
+                    prize = format_prize(cells[2].text)
+                
+                draw["num_1"] = str(drawn_numbers[0])
+                draw["num_2"] = drawn_numbers[1]
+                draw["num_3"] = drawn_numbers[2]
+                draw["num_4"] = drawn_numbers[3]
+                draw["num_5"] = drawn_numbers[4]
+                draw["bonus_1"] = drawn_numbers[5]
+                draw["bonus_2"] = drawn_numbers[6]
+                draw["millionaire_maker_code"] = maker_code
+                draw["jackpot"] = prize[0]
+                draw["rollover"] = prize[1]
+
+            elif lottery_type == "lotto":
+                prize = format_prize(cells[2].text)
+
+                draw["num_1"] = str(drawn_numbers[0])
+                draw["num_2"] = drawn_numbers[1]
+                draw["num_3"] = drawn_numbers[2]
+                draw["num_4"] = drawn_numbers[3]
+                draw["num_5"] = drawn_numbers[4]
+                draw["num_6"] = drawn_numbers[5]
+                draw["bonus_1"] = drawn_numbers[6]
+                draw["jackpot"] = prize[0]
+                draw["rollover"] = prize[1]
 
             data.append(draw)
 
-        
-    export_to_xlsx("lotto", data)
+        return data
 
 
 def export_to_xlsx(filename, data):
-    df = pd.DataFrame(data)
+    formatted_data = []
+
+    for year in data:
+        for draw in year:
+            formatted_data.append(draw)
+
+    df = pd.DataFrame(formatted_data)
     df.to_excel(filename + ".xlsx")
 
 
